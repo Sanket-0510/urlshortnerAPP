522f2fd9968c4fc7420fd9a55f3bf407fee63faf
sanket <telisanket2002@gmail.com>
2023-08-04 23:35:53 +0530

    succesesfully tested the get,post,patch and delete userRoutes

--- connection.js
+++ connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 async function connect(url){
-    return mongoose.connect(url)
+    return mongoose.connect(url,{useNewUrlParser: true,
+        useUnifiedTopology: true,})
 }
 
 module.exports = connect
